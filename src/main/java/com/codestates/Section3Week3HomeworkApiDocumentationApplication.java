9ad1210e2c2eee28226786803390dd0a5150d17f
hjs6877 <hjs6877@gmail.com>
2022-12-12 08:53:52 +0900

    .

--- src/main/java/com/codestates/Section3Week3HomeworkApiDocumentationApplication.java
+++ src/main/java/com/codestates/Section3Week3HomeworkApiDocumentationApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class Be39Section3Week3HomeworkApiDocumentationApplication {
+public class Section3Week3HomeworkApiDocumentationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Be39Section3Week3HomeworkApiDocumentationApplication.class, args);
+		SpringApplication.run(Section3Week3HomeworkApiDocumentationApplication.class, args);
 	}
 
 }
--- src/test/java/com/codestates/Section3Week3HomeworkApiDocumentationApplicationTests.java
+++ src/test/java/com/codestates/Section3Week3HomeworkApiDocumentationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Be39Section3Week3HomeworkApiDocumentationApplicationTests {
+class Section3Week3HomeworkApiDocumentationApplicationTests {
 
 	@Test
 	void contextLoads() {
