935c66c98facf7ff03175d20ec5af37d7a17430b
hjs6877 <hjs6877@gmail.com>
2022-12-12 11:44:55 +0900

    Modify post handlers

--- src/test/java/com/codestates/restdocs/member/MemberControllerRestDocsTest.java
+++ src/test/java/com/codestates/restdocs/member/MemberControllerRestDocsTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,16 +24,18 @@ import java.util.List;
 
 import static com.codestates.util.ApiDocumentUtils.getRequestPreProcessor;
 import static com.codestates.util.ApiDocumentUtils.getResponsePreProcessor;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MemberController.class)
 @MockBean(JpaMetamodelMappingContext.class)
@@ -56,8 +59,8 @@ public class MemberControllerRestDocsTest {
         MemberDto.Post post = new MemberDto.Post("hgd@gmail.com", "홍길동", "010-1234-5678");
         String content = gson.toJson(post);
 
-        MemberDto.response responseDto =
-                new MemberDto.response(1L,
+        MemberDto.Response responseDto =
+                new MemberDto.Response(1L,
                         "hgd@gmail.com",
                         "홍길동",
                         "010-1234-5678",
@@ -67,7 +70,9 @@ public class MemberControllerRestDocsTest {
         // willReturn()이 최소한 null은 아니어야 한다.
         given(mapper.memberPostToMember(Mockito.any(MemberDto.Post.class))).willReturn(new Member());
 
-        given(memberService.createMember(Mockito.any(Member.class))).willReturn(new Member());
+        Member mockResultMember = new Member();
+        mockResultMember.setMemberId(1L);
+        given(memberService.createMember(Mockito.any(Member.class))).willReturn(mockResultMember);
 
         given(mapper.memberToMemberResponse(Mockito.any(Member.class))).willReturn(responseDto);
 
@@ -83,13 +88,10 @@ public class MemberControllerRestDocsTest {
         // then
         actions
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.data.email").value(post.getEmail()))
-                .andExpect(jsonPath("$.data.name").value(post.getName()))
-                .andExpect(jsonPath("$.data.phone").value(post.getPhone()))
+                .andExpect(header().string("Location", is(startsWith("/v11/members/"))))
                 .andDo(document("post-member",
                         getRequestPreProcessor(),
                         getResponsePreProcessor(),
-                        // request body
                         requestFields(
                                 List.of(
                                         fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
@@ -97,17 +99,8 @@ public class MemberControllerRestDocsTest {
                                         fieldWithPath("phone").type(JsonFieldType.STRING).description("휴대폰 번호")
                                 )
                         ),
-                        // response body
-                        responseFields(
-                                List.of(
-                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
-                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
-                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
-                                        fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름"),
-                                        fieldWithPath("data.phone").type(JsonFieldType.STRING).description("휴대폰 번호"),
-                                        fieldWithPath("data.memberStatus").type(JsonFieldType.STRING).description("회원 상태"),
-                                        fieldWithPath("data.stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수")
-                                )
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("Location header. 등록된 리소스의 URI")
                         )
                 ));
     }
@@ -119,8 +112,8 @@ public class MemberControllerRestDocsTest {
         MemberDto.Patch patch = new MemberDto.Patch(memberId, "홍길동", "010-1111-1111", Member.MemberStatus.MEMBER_ACTIVE);
         String content = gson.toJson(patch);
 
-        MemberDto.response responseDto =
-                new MemberDto.response(1L,
+        MemberDto.Response responseDto =
+                new MemberDto.Response(1L,
                         "hgd@gmail.com",
                         "홍길동",
                         "010-1111-1111",
