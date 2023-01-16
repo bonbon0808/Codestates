7f4dc5e982e2d675bebbf7bc0f4ee824d2d46f99
hjs6877 <hjs6877@gmail.com>
2023-01-16 11:55:37 +0900

    .

--- src/main/java/com/codestates/auth/HelloUserAuthenticationProvider.java
+++ src/main/java/com/codestates/auth/HelloUserAuthenticationProvider.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.Collection;
 import java.util.Optional;
 
-@Component
+//@Component
 public class HelloUserAuthenticationProvider implements AuthenticationProvider {
     private final HelloUserDetailsServiceV3 userDetailsService;
     private final PasswordEncoder passwordEncoder;
