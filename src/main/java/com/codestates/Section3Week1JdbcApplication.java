9627de0624a3fc6c460388b1d04381e872dabc6d
hjs6877 <hjs6877@gmail.com>
2023-02-21 00:14:06 +0900

    .

--- src/main/java/com/codestates/Section3Week1JdbcApplication.java
+++ src/main/java/com/codestates/Section3Week1JdbcApplication.java
@@ -1,17 +1,7 @@
 package com.codestates;
 
-import com.codestates.order.dto.OrderCoffeeDto;
-import com.codestates.order.dto.OrderPostDto;
-import com.codestates.order.entity.Order;
-import com.codestates.order.mapper.OrderMapper;
-import com.codestates.order.service.OrderService;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Section3Week1JdbcApplication {
@@ -20,16 +10,4 @@ public class Section3Week1JdbcApplication {
 		SpringApplication.run(Section3Week1JdbcApplication.class, args);
 	}
 
-//	@Bean
-//	public ApplicationRunner testJpaBasicRunner(OrderMapper orderMapper, OrderService orderService) {
-//
-//		return args -> {
-//			List<OrderCoffeeDto> orderCoffeeDtoList = List.of(
-//					new OrderCoffeeDto(1, 2)
-//			);
-//			OrderPostDto orderPostDto = new OrderPostDto(1, orderCoffeeDtoList);
-//			Order order = orderMapper.orderPostDtoToOrder(orderPostDto);
-//			orderService.createOrder(order);
-//		};
-//	}
 }
