1ff89f86ae6da0751b18e977d1c48fb611d04dc5
hjs6877 <hjs6877@gmail.com>
2023-02-27 13:25:18 +0900

    .

--- src/main/java/com/codestates/member/dto/MemberResponseDto.java
+++ src/main/java/com/codestates/member/dto/MemberResponseDto.java
@@ -13,10 +13,10 @@ public class MemberResponseDto {
     private String name;
     private String phone;
     private Member.MemberStatus memberStatus;   // TODO 추가된 부분
-//    private String memberStatus;   // Mapper에 매핑 시
+//    private String memberStatus;   // Mapper에서 매핑 시
 
     // TODO 수정된 부분
-    public String getMemberStatus() {
-        return memberStatus.getStatus();
-    }
+//    public String getMemberStatus() {
+//        return memberStatus.getStatus();
+//    }
 }
