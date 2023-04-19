a697b6fb775591d18c6bc1d59c9bcc7819a75b8a
hjs6877 <jungsik.hwang@codestates.com>
2023-04-19 14:23:10 +0900

    Add a comment

--- src/main/java/com/codestates/order/entity/CoffeeRef.java
+++ src/main/java/com/codestates/order/entity/CoffeeRef.java
@@ -11,6 +11,11 @@ import org.springframework.data.relational.core.mapping.Table;
 public class CoffeeRef {
     @Id
     private long orderCoffeeId;
+
+    // 왜 AggregateRef로 감싸지 않는가? --> Order에서 CoffeeRef에 이미 접근 가능하므로..
+    // 이것만 기억합시다. -> 어떤 객체에 접근하기 위해서는 접근하려는 객체의 참조값만 있으면 된다.
     private long coffeeId;
     private int quantity;
+
+    // 왜 private long orderId는 없는가? --> Order가 private Set<CoffeeRef> orderCoffees로 이미 참조 가능하므로..
 }
