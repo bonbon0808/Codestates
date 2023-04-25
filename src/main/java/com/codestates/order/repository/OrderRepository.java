e110a7388960564b7454f0579c0fe46ebee06371
hjs6877 <jungsik.hwang@codestates.com>
2023-04-25 18:45:04 +0900

    .

--- src/main/java/com/codestates/order/repository/OrderRepository.java
+++ src/main/java/com/codestates/order/repository/OrderRepository.java
@@ -3,5 +3,5 @@ package com.codestates.order.repository;
 import com.codestates.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> { // 추가
 }
