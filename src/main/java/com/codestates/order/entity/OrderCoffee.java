b6b85349dbb24a729f1e6aa1c45c4b8ec2c7d20b
hjs6877 <jungsik.hwang@codestates.com>
2023-05-10 17:14:59 +0900

    .

--- src/main/java/com/codestates/order/entity/OrderCoffee.java
+++ src/main/java/com/codestates/order/entity/OrderCoffee.java
@@ -11,14 +11,10 @@ import org.springframework.data.relational.core.mapping.Table;
 public class OrderCoffee {
     @Id
     private long orderCoffeeId;
-
-    // 왜 AggregateReference로 감싸지 않는가?
-    // --> Coffee와 CoffeeRef는 애그리거트 루트 간의 관계가 아니다.
-    // --> CoffeeRef 자체가 coffeeId를 감싼 참조 객체의 역할을 한다.
     private long coffeeId;
     private int quantity;
 
     // 왜 private long orderId는 없는가?
     // --> Order가 private Set<CoffeeRef> orderCoffees로 이미 참조 가능하므로..
-    // --> 즉, CoffeeRef가 이미 Order와 외래키의 관계를 가진다는 의미임.
+    // --> 즉, OrderCoffee가 이미 Order와 외래키의 관계를 가진다는 의미임.
 }
