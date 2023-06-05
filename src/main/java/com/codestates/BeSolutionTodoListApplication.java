986f905d636b10d49974ff792254f449c7bbf8dd
hjs6877 <jungsik.hwang@codestates.com>
2023-06-05 17:15:07 +0900

    .

--- src/main/java/com/codestates/BeSolutionTodoListApplication.java
+++ src/main/java/com/codestates/BeSolutionTodoListApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class BeTodoApplication {
+public class BeSolutionTodoListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeTodoApplication.class, args);
+		SpringApplication.run(BeSolutionTodoListApplication.class, args);
 	}
 
 }
--- src/main/java/com/codestates/ServletInitializer.java
+++ src/main/java/com/codestates/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BeTodoApplication.class);
+		return application.sources(BeSolutionTodoListApplication.class);
 	}
 
 }
