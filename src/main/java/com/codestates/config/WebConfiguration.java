aad5ccbdd4a8286ad08c6fefd17153b35230957d
hjs6877 <jungsik.hwang@codestates.com>
2023-06-07 14:57:48 +0900

    Remove CorsInterceptor

--- src/main/java/com/codestates/config/WebConfiguration.java
+++ src/main/java/com/codestates/config/WebConfiguration.java
@@ -1,18 +1,11 @@
 package com.codestates.config;
 
-import com.codestates.interceptor.CorsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration  implements WebMvcConfigurer {
-    // Interceptor를 이용할 수도 있음.
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new CorsInterceptor()).addPathPatterns("/**");
-//    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/v1/todos")
