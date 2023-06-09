b478fe161157fbc584d05b31c7a6f39dcb33f35c
hjs6877 <jungsik.hwang@codestates.com>
2023-06-09 11:54:08 +0900

    .

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -2,3 +2,4 @@
 # Change ownership of files in /home/ubuntu/build directory using sudo
 sudo chown -R ubuntu:ubuntu /home/ubuntu/build
 sudo chmod +x /home/ubuntu/build/**
+sudo mkdir "/var/log/coffee-order-app/app.log"
